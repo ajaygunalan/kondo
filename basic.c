4e99962987938507446152545a699c1254a7b668
ajay <ajaygunalan1995@gmail.com>
2018-08-29 00:23:42 -0900

    corrected serial port

--- basic.c
+++ basic.c
@@ -9,7 +9,7 @@ int main ()
   int serial_port ;
   char dat;
 
-  if ((serial_port = serialOpen ("ttyS0", 115200)) < 0)  /* open serial port */
+  if ((serial_port = serialOpen ("/dev/ttyS0", 115200)) < 0)  /* open serial port */
   {
     fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
     return 1 ;
